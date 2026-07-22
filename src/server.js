8456295aa94ba49d037c3edfef89b9ce9f56336b
Victor Martinez <vicmarc30@gmail.com>
2026-07-22 16:15:03 -0600

    Add cron job to send GET request every 14 minutes

--- src/server.js
+++ src/server.js
@@ -4,17 +4,24 @@ import rateLimiter from './middleware/rateLimiter.js'
 
 import { initDB } from './config/db.js'
 
+import job from './config/cron.js'
 import transactionsRoute from './routes/transactionsRoute.js'
 
 dotenv.config()
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') job.start()
+
 // Middleware
 app.use(rateLimiter) // Apply rate limiting middleware to all routes
 app.use(express.json())
 
 const PORT = process.env.PORT || 5001
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" })
+})
 
 app.use('/api/transactions', transactionsRoute)
 // app.use("/api/products", transactions)
